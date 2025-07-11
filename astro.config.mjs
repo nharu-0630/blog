a597469b594eb39b45672f0d33036445626aecfa
nharu-0630 <root@nharu.dev>
2025-07-11 20:59:36 +0900

    [fix] configのsiteを修正

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://blog.nharu.dev',
+	site: 'https://nharu.dev',
 	output: 'static',
 	integrations: [mdx(), sitemap()],
 });
